3ee95c726a80b432cccbc7490230cea81ae89af6
Diana <artemida91510191@gmail.com>
2021-04-14 00:52:31 +0200

    api for a text joke and for an img

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+const request = require('request');
 
 export default function Main() {
+  const [joke, setJoke] = useState('');
+  let options = {
+    url: 'https://geek-jokes.sameerkumar.website/api?format=json',
+    method: 'GET'
+  }
+  const onClickHandler = () => {
+    request(options, (err, response, body) => {
+      if (!err && response.statusCode === 200)
+        console.log(body)
+      setJoke(body)
+    });
+  }
+
   return (
     <div>
-      Main hello msg!
+      <button
+        className='button-new-joke'
+        onClick={() => onClickHandler()}
+      >
+        New Joke, please!
+          </button>
+      <div className="text-joke">{joke}</div>
     </div>
   )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+// const unirest = require('unirest');
+
+const request = require('request');
 
 export default function Home() {
+  const [meme, setMeme] = useState('');
+  // const req = unirest("GET", "https://ronreiter-meme-generator.p.rapidapi.com/meme");
+
+  // req.query({
+  //   "meme": "Developer",
+  //   "bottom": "I am a developer!",
+  //   "top": "hello, honey,",
+  //   "font_size": "50",
+  //   "font": "Impact"
+  // });
+
+  // req.headers({
+  //   "x-rapidapi-key": "4e5a35a4b1mshf5b63a44736fa7ep1acba8jsnfa1d45389e90",
+  //   "x-rapidapi-host": "ronreiter-meme-generator.p.rapidapi.com",
+  //   "useQueryString": true
+  // });
+
+  // const onClickMemeHandler = () => {
+  //   req.end(function (res) {
+  //     if (res.error) throw new Error(res.error);
+
+  //     console.log(res.body);
+  //   });
+  // }
+
+  let options = {
+    url: 'https://api.imgflip.com/get_memes',
+    method: 'GET'
+  }
+  const onClickMemeHandler = () => {
+    request(options, (err, response, body) => {
+      if (!err && response.statusCode === 200) {
+        let randomNumber = Math.floor(Math.random() * 100);
+        let a = JSON.parse(body)["data"]["memes"][randomNumber]["url"];
+        console.log(a)
+        setMeme(a)
+      }
+    });
+  }
+
   return (
     <div>
-      Home
+      <button
+        className='button-new-joke'
+        onClick={() => onClickMemeHandler()}
+      >
+        New Meme, please!
+          </button>
+      <br />
+      <img src={meme} alt="meme" />
     </div>
   )
 }
